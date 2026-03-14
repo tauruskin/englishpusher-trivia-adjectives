23fb96d305d62d730183c2699f6d97896160c4eb
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2026-03-14 15:18:34 +0200

    Update index.ts

--- src/data/topics/index.ts
+++ src/data/topics/index.ts
@@ -2,6 +2,7 @@ import { WordEntry } from "@/data/wordList";
 import adjectives from "./adjectives";
 import stativeVerbs from "./stative-verbs";
 import personalityAdjectives from "./personality-adjectives";
+import adverbsOfFrequency from "./adverbs-of-frequency";
 
 export interface Topic {
   id: string;
@@ -13,6 +14,7 @@ const topics: Topic[] = [
   { id: "adjectives", name: "Adjectives for Feelings", wordList: adjectives },
   { id: "stative-verbs", name: "Stative Verbs", wordList: stativeVerbs },
   { id: "personality-adjectives", name: "Personality Adjectives", wordList: personalityAdjectives },
+  { id: "adverbs-of-frequency", name: "Adverbs of Frequency", wordList: adverbsOfFrequency },
 ];
 
 export default topics;
